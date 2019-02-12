b02899d37a9151d3e686409a03b72379c05d7126
Michal Wierzbinski <michal@novoda.com>
2019-02-12 15:30:53 +0000

    Added Details view controller class.

---
+++ NYTimesArticles/ArticleDetailsScene/ArticleDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleDetailsViewController.swift
+//  NYTimesArticles
+//
+//  Created by Michal W on 12/02/2019.
+//  Copyright © 2019 Michal W. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class ArticleDetailsViewController: UIViewController {
+    
+}
