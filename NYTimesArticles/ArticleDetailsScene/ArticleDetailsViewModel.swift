ae4ac2bbac8eed10e1f3412730b97eb6710ce988
Michal Wierzbinski <michal@novoda.com>
2019-02-12 16:53:24 +0000

    Implemented details view model.

--- NYTimesArticles/ArticleDetailsScene/ArticleDetailsViewModel.swift
+++ NYTimesArticles/ArticleDetailsScene/ArticleDetailsViewModel.swift
@@ -9,5 +9,24 @@
 import Foundation
 
 final class ArticleDetailsViewModel {
+    private let article: Article
     
+    var detailsViewData: DetailsViewData {
+        return DetailsViewData(title: article.title,
+                               abtract: article.abstract,
+                               section: article.section)
+    }
+    
+    init(article: Article) {
+        self.article = article
+    }
+    
+    
+}
+
+struct DetailsViewData {
+    let title: String
+    let abtract: String
+    let section: String
+    //TODO: add image
 }
