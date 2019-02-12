a5cbc3676412476e9d29be406f5c7fa5e32fb024
Michal Wierzbinski <michal@novoda.com>
2019-02-12 16:55:49 +0000

    Implemented article details VIew controller.

--- NYTimesArticles/ArticleDetailsScene/ArticleDetailsViewController.swift
+++ NYTimesArticles/ArticleDetailsScene/ArticleDetailsViewController.swift
@@ -9,6 +9,41 @@
 import Foundation
 import UIKit
 
+protocol CustomSegueIdentifiable {
+    static var customSegueIdentifier: String { get }
+}
+
+extension UIViewController: CustomSegueIdentifiable {
+    static var customSegueIdentifier: String {
+        return String(describing: self)
+    }
+}
+
 final class ArticleDetailsViewController: UIViewController {
     
+    @IBOutlet weak var articleTitle: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var abstractInfo: UILabel!
+    @IBOutlet weak var section: UILabel!
+    
+    private var viewModel: ArticleDetailsViewModel?
+    private var data: DetailsViewData?
+        
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let data = data else { return }
+        articleTitle.text = data.title
+        abstractInfo.text = data.abtract
+        section.text = data.section
+    }
+    
+    func update(with article: Article) {
+        viewModel = ArticleDetailsViewModel(article: article)
+        data = viewModel?.detailsViewData
+    }
 }
