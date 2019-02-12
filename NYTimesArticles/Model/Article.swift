5ab985ba1d6bf3c5ecd79a83514b012825935496
Michal Wierzbinski <michal@novoda.com>
2019-02-12 16:52:33 +0000

    Added abstract and section to Article model

--- NYTimesArticles/Model/Article.swift
+++ NYTimesArticles/Model/Article.swift
@@ -11,12 +11,16 @@ import Foundation
 struct Article: Codable {
     let title: String
     let byline: String
+    let abstract: String
+    let section: String
     let publishedDate: Date
     
     private enum CodingKeys: String, CodingKey
     {
         case title
         case byline
+        case abstract
+        case section
         case publishedDate = "published_date"
     }
 }
